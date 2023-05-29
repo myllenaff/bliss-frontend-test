4a2d4b6c415145a8ae14b12bbfe934e2febd6310
Myllena Freitas <myllenaff@gmail.com>
2023-05-29 20:08:18 +0100

    chore: Create Details page

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,21 @@
+import { Container } from './styles';
+
+
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
+
+export function Details() {
+  
+  
+  return (
+    <Container>
+
+      <Header/>
+
+      
+      <Button title="Show More"/>
+      
+
+    </Container>
+  )
+}
