88a72511b81ff7ff34ca5a7567c783eea517bdca
Myllena Freitas <myllenaff@gmail.com>
2023-05-29 20:10:06 +0100

    feat: Change main file to receive styles

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Details } from './pages/Details';
+
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/global';
+
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode >
+    <ThemeProvider theme={theme}>
+      <GlobalStyle/>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )
