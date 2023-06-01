aabe0e7c5735c63af0292649f831296f282ac726
Myllena Freitas <myllenaff@gmail.com>
2023-06-01 19:33:31 +0100

    feat: Implement health check

--- src/pages/Loading/index.jsx
+++ src/pages/Loading/index.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import { ContainerLoading } from './styled';
 
 export function Loading() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(
+      'https://private-anon-0fd6771c03-blissrecruitmentapi.apiary-mock.com/health'
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.status === 'OK') return navigate('/questions');
+        if (json.status !== 'OK') return navigate('/not-found');
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <ContainerLoading>
       <p>Loading...</p>
