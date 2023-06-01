91d9cc1ccdc40d2abae1f2a8671ce54fed33b21b
Myllena Freitas <myllenaff@gmail.com>
2023-06-01 19:29:27 +0100

    refactor: Add title param

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Container } from './styled';
 
-export function Header() {
+export function Header({ title }) {
   return (
     <Container>
-      <div>
-        Questions list
-      </div>
+      <div>{title}</div>
     </Container>
-  )
+  );
 }
