f32139ce55b8f2337d0cf49e1c3554c9452bc55b
Myllena Freitas <myllenaff@gmail.com>
2023-06-06 11:47:46 +0100

    feat: Api call

--- src/pages/Questions/index.jsx
+++ src/pages/Questions/index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+
 import { ButtonText } from '../../components/ButtonText';
 import { RiShareFill } from 'react-icons/ri';
 import { FiSearch } from 'react-icons/fi';
@@ -13,14 +15,28 @@ import {
   QuestionsList,
   ButtonSearchTop,
 } from './styled';
-import { Question } from '../../components/Question';
-// import { questionMock } from '../../mock/questions';
-// import { ButtonVote } from '../../components/ButtonVote';
-
-// import { formatData } from '../../helpers/formatData';
-// import { BsCalendar2 } from 'react-icons/bs';
+import Question from '../../components/Question';
 
 export function Questions() {
+  const notify = () => toast.error('No more questions!', { duration: 1000 });
+
+  const [questionsAPI, setQuestionsAPI] = useState([]);
+
+  const limit = 10;
+  const offset = 10;
+  const filter = 'x';
+
+  useEffect(() => {
+    fetch(
+      `https://private-anon-16b652f098-blissrecruitmentapi.apiary-mock.com/questions?limit=${limit}&offset=${offset}&filter=${filter}`
+    )
+      .then((response) => response.json())
+      .then((json) => {
+        return setQuestionsAPI(json);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <ContainerHome>
       <Header title='Questions list' />
@@ -31,13 +47,21 @@ export function Questions() {
             <ButtonText title='Share' icon={RiShareFill} />
           </ButtonSearchTop>
         </Search>
-
         <QuestionsList>
-          <Question />
-          <Question />
+          {questionsAPI.map((question, index) => (
+            <Question
+              key={index}
+              id={question.id}
+              title={question.question}
+              image={question.thumb_url}
+              choices={question.choices}
+            />
+          ))}
         </QuestionsList>
-
-        <Button title='Show More' />
+        <div>
+          <Button title='Show More' onClick={notify} />
+          <Toaster position='bottom-center' reverseOrder={false} />
+        </div>
       </SectionHome>
     </ContainerHome>
   );
